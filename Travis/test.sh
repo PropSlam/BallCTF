4b6c67506adc2becdf95d9f1afc58a0c29beae2c
shoffing <shoffing@gmail.com>
2019-11-12 20:12:20 -0500

    batch mode, no graphics

--- Travis/test.sh
+++ Travis/test.sh
@@ -2,6 +2,8 @@
 
 echo "Attempting to run tests."
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+    -batchmode \
+    -nographics \
     -runTests \
     -testPlatform playmode \
     -projectPath $(pwd) \
