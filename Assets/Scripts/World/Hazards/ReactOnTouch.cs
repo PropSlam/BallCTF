71bfaffa087ebf9849abc9266e2dcb5d70515c8e
Charlie <chaoticcow@gmail.com>
2019-11-14 15:38:43 -0500

    Add Barebones spike trap and tests
    Spike Trap
    Scene for Charlie to work in
    Basic Color Materials

---
+++ Assets/Scripts/World/Hazards/ReactOnTouch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactOnTouch : MonoBehaviour
+{
+    public LayerMask reactLayer;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) {
+            other.GetComponent<Player>().Death(this);
+        }
+    }
+}
