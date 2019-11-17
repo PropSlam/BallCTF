00c33a08a08cb97e8448cf6749b4b0d3b35b95e5
shoffing <shoffing@gmail.com>
2019-11-17 12:14:52 -0500

    implement player movement with tests using new Input System

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.UI;
-using UniRx;
-using UniRx.Triggers;
 
 public class Player : MonoBehaviour {
+    private const float MovementSpeed = 1.8f;
+
     public TeamReactiveProperty team;
     public StringReactiveProperty alias;
+    private new Rigidbody rigidbody;
     private Text aliasText;
 
     internal void Death() {
@@ -15,6 +16,8 @@ public class Player : MonoBehaviour {
     }
 
     void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+
         // Instantiate Alias text into main UI canvas and clean up with OnDestroy
         aliasText = GameObject.Instantiate(Resources.Load<Text>("Prefabs/Player/Alias"));
         aliasText.transform.SetParent(GameObject.FindGameObjectWithTag("UI").transform);
@@ -30,9 +33,24 @@ public class Player : MonoBehaviour {
 
         // Subscribe to alias changes.
         alias.Subscribe(newAlias => aliasText.text = newAlias);
+
+        // Subscribe to inputs.
+        var actionEvents = GetComponent<UnityEngine.InputSystem.PlayerInput>().actionEvents;
+
+        // Movement inputs.
+        var playerMoveEvent = actionEvents[actionEvents.IndexOf(action => action.actionName == "Player/Move")];
+        var playerMove = playerMoveEvent.AsObservable().Select(context => context.ReadValue<Vector2>());
+        gameObject.UpdateAsObservable()
+            .WithLatestFrom(playerMove, (_, inputVec) => inputVec)
+            .Subscribe(inputVec => {
+                var moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+                var moveForce = moveVec * MovementSpeed;
+                rigidbody.AddForce(moveForce, ForceMode.Acceleration);
+            });
     }
 
     void OnGUI() {
+        // Move alias text to correct position.
         var aliasPos = transform.position + Camera.main.transform.up;
         var aliasScreenPos = Camera.main.WorldToScreenPoint(aliasPos);
         aliasText.rectTransform.position = aliasScreenPos;
--- Assets/Tests/PlayerTest.cs
+++ Assets/Tests/PlayerTest.cs
@@ -3,13 +3,16 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.TestTools;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace Tests {
-    public class PlayerTest {
+    public class PlayerTest : InputTestFixture {
         private Player player;
+        private Keyboard keyboard;
 
         [SetUp]
-        public void Setup() {
+        public new void Setup() {
             // Spawn main camera.
             var cam = GameObject.Instantiate(
                 new GameObject("Camera"),
@@ -22,10 +25,17 @@ namespace Tests {
             // Spawn UI.
             GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/UI"));
 
+            // Spawn floor plane.
+            var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            plane.transform.localScale = new Vector3(10, 10, 10);
+
             // Load player prefab.
             var playerPrefab = Resources.Load<GameObject>("Prefabs/Player/Player");
-            var playerObj = GameObject.Instantiate(playerPrefab);
+            var playerObj = GameObject.Instantiate(playerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
             player = playerObj.GetComponent<Player>();
+
+            // Initialize keyboard input
+            keyboard = InputSystem.AddDevice<Keyboard>();
         }
 
         // Tests material is set to Yellow when team is Yellow.
@@ -79,6 +89,46 @@ namespace Tests {
             var expectedScreenPos = Camera.main.WorldToScreenPoint(expectedAliasPos);
             var differenceToExpected = Vector3.Distance(expectedScreenPos, aliasText.rectTransform.position);
             Assert.Less(differenceToExpected, 0.01f); // close enough...
+        }
+
+        private IEnumerator MoveWithKey(KeyControl key) {
+            Time.timeScale = 10f;
+            Press(key);
+            yield return new WaitForSeconds(1);
+            Release(key);
+            Time.timeScale = 1f;
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerMovesLeft() {
+            var oldPos = player.transform.position.x;
+            yield return MoveWithKey(keyboard.leftArrowKey);
+            var newPos = player.transform.position.x;
+            Assert.Less(newPos - oldPos, -0.1);
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerMovesRight() {
+            var oldPos = player.transform.position.x;
+            yield return MoveWithKey(keyboard.rightArrowKey);
+            var newPos = player.transform.position.x;
+            Assert.Greater(newPos - oldPos, 0.1);
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerMovesDown() {
+            var oldPos = player.transform.position.z;
+            yield return MoveWithKey(keyboard.downArrowKey);
+            var newPos = player.transform.position.z;
+            Assert.Less(newPos - oldPos, -0.1);
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerMovesUp() {
+            var oldPos = player.transform.position.z;
+            yield return MoveWithKey(keyboard.upArrowKey);
+            var newPos = player.transform.position.z;
+            Assert.Greater(newPos - oldPos, 0.1);
         }
     }
 }
